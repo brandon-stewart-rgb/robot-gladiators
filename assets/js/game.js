3bec16a760cbf10867b5a86e0fe15e249e619799
brandon-stewart-rgb <brandon@pixeltree.us>
2021-06-28 13:39:56 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,11 @@ var playerMoney = 10;
 // You can also log multiple values at once lie this 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting a round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -22,20 +22,20 @@ var fight = function() {
         // remove enemy's health by subtracting the amount set in the playerAttack
         enemyHealth = enemyHealth -playerAttack;
         console.log(
-            playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."      
+            playerName + " attacked " + enemyNames + " . " + enemyNames + " now has " + enemyHealth + " health remaining."      
             );
     
         //check enemy's health 
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyNames + " has died!");
         } else {
-          window.alert(enemyName + " still has " + enemyHealth + " health left.");
+          window.alert(enemyNames + " still has " + enemyHealth + " health left.");
         }
 
         //remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has "  + playerHealth + " health remaining."
+            enemyNames + " attacked " + playerName + ". " + playerName + " now has "  + playerHealth + " health remaining."
         );
 
         // check player's health
@@ -68,4 +68,8 @@ var fight = function() {
 };
 
 // Run fight function
-fight();
+ fight();
+
+for (let i = 0; i < enemyNames.length; i++) {
+   fight(enemyNames[i]);s 
+}
